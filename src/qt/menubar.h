448d39bc701d47a4def221d90574784838992121
justthedoctor <justthedoctor.931@gmail.com>
2019-06-21 13:50:04 -0500

    Update menubar.h

--- src/qt/menubar.h
+++ src/qt/menubar.h
@@ -24,7 +24,7 @@ signals:
     void showSettingsMenu(QPoint);
     void showHelpMenu(QPoint);
     void showAddressBook(QPoint);
-    void showDonationBook(QPoint);
+    void showDonateBook(QPoint);
     
 public slots:
     void clientModeChanged(ClientMode);
